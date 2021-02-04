0928da8d7d3e1d7162d0b9f32484ab8da39335e3
Amadou <xalima99@gmail.com>
2021-02-04 11:15:45 +0000

    rate limiter added to root app using slowapi

--- app/server/app.py
+++ app/server/app.py
@@ -1,15 +1,22 @@
 """Pin Api Root Path"""
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from fastapi_versioning import VersionedFastAPI
+#Imports for rate limiter
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
 from app.server.routes.v1.pin import router as PinRouter_v1
 from app.server.routes.v1.user import router as UserRouter_v1
 
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
-
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 @app.get("/", tags=["Root"])
-async def read_root():
+@limiter.limit("5/minute")
+async def read_root(request: Request, response: Response):
     """Root path, Pin Api available at  http://localhost:8000/v1/docs#/"""
     return {"message": "Welcome to this fantastic app!"}
 
